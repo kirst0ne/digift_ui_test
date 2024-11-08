70952cd5b9d0d6ea5d158fa49f2f28c64e26a126
Kirill Seleznev <okolo66@yandex.ru>
2024-11-08 23:32:51 +0300

    Добавление ссылки на драйвер во вспомогательные методы

--- test_digift_ui.py
+++ test_digift_ui.py
@@ -8,13 +8,15 @@ class digift_ui(unittest.TestCase):
         self.wd = webdriver.Chrome(executable_path='C:/Windows/System32/chromedriver.exe')
         self.wd.implicitly_wait(30)
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://qa.digift.ru/")
 
     def scroll_to_element(self, element):
         self.wd.execute_script("arguments[0].scrollIntoView();", element)
 
-    def choose_cards(self, wd):
+    def choose_cards(self):
+        wd = self.wd
         wd.find_element_by_xpath("//li[1]/button").click()
         wd.find_element_by_xpath("//li[2]/button").click()
         wd.find_element_by_xpath("//li[3]/button").click()
@@ -22,12 +24,14 @@ class digift_ui(unittest.TestCase):
         wd.find_element_by_xpath("//li[5]/button").click()
         wd.find_element_by_xpath("//li[6]/button").click()
 
+    def find_nominal_card_element(self):
+        return self.wd.find_element_by_xpath("//*[text()='Номинал карты']")
+
     def test_digift_ui(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        nominal_card_element = wd.find_element_by_xpath("//*[text()='Номинал карты']")
+        self.open_home_page()
+        nominal_card_element = self.find_nominal_card_element()
         self.scroll_to_element(nominal_card_element)
-        self.choose_cards(wd)
+        self.choose_cards()
 
     def is_element_present(self, how, what):
         try:
