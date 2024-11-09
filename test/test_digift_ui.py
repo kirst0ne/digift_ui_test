89a44a543244817bb79c071ba18a324ac1828237
Kirill Seleznev <okolo66@yandex.ru>
2024-11-09 18:14:57 +0300

    Реализованы Allure декораторы

--- test/test_digift_ui.py
+++ test/test_digift_ui.py
@@ -1,7 +1,13 @@
+import allure
+
+
 def test_digift_ui(app):
-    nominal_card_element = app.cards.find_nominal_card_element()
-    app.cards.scroll_to_element(nominal_card_element)
-    app.cards.activate_initial_card()
-    assert app.cards.is_activation_button_enabled()
-    app.cards.choose_cards(app.wd)
-    assert app.cards.is_activation_button_enabled()
+    with allure.step('Finding of card nominals'):
+        nominal_card_element = app.cards.find_nominal_card_element()
+        app.cards.scroll_to_element(nominal_card_element)
+    with allure.step('Check that the button is active'):
+        app.cards.activate_initial_card()
+        assert app.cards.is_activation_button_enabled()
+    with allure.step('The buttons are activated and the selected denomination is displayed'):
+        app.cards.choose_cards(app.wd)
+        assert app.cards.is_activation_button_enabled()
